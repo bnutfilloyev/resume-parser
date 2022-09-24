76b033bf7da7646a6b8b12fd375db40de5a4f0a6
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-09-24 14:19:28 +0300

    database removed

--- loader.py
+++ loader.py
@@ -1,9 +1,8 @@
+import nltk
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import nltk
 
 from config import load_config
-from utils.database import MongoDB
 
 nltk.download('stopwords')
 
@@ -11,4 +10,3 @@ config = load_config()
 bot = Bot(token=config.bot.token, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-db = MongoDB()
